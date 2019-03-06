22a184c8d52459eda62ecb48501444e1f3686497
vireah <vireah@gmail.com>
2019-03-06 18:07:32 +0300

    feat: fist implemenе for rating)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brackets = bracketsConfig.join(',').replace(/,/g, '');
+  brackets = brackets.split('');
+
+  function peek(){
+      return stack[stack.length-1];
+  }
+  
+  for(let i=0; i<str.length; i++){
+    let temp = str.charAt(i);
+    for(let j=1; j<brackets.length; j++){
+      let a =  peek();
+      let b = brackets[j-1];
+      if(temp === brackets[j] && a === brackets[j-1]){
+        stack.pop(temp);
+      } else stack.push(temp);
+    }
+  }
+  if (stack.length === 0) {
+      return true
+  } else return false;
 }
